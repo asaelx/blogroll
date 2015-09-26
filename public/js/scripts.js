1c19b0a2354063c6d1d00edf9c23eb006bec21b1
Asael Chávez <asaelx@gmail.com>
2015-09-26 13:50:17 -0500

    Comments

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,5 +1,7 @@
+//Since Backbone gets its id values with 'id', change to '_id' like MongoDB
 Backbone.Model.prototype.idAttribute = '_id';
 
+//Declare Model in Backbone
 var Blog = Backbone.Model.extend({
     defaults: {
         author: '',
@@ -8,34 +10,45 @@ var Blog = Backbone.Model.extend({
     }
 });
 
+//Declare Colletion in Backbone
 var Blogs = Backbone.Collection.extend({
+    //Set url for collection
     url: "http://localhost:3000/api/blogs"
 });
 
+//Assign Collection in blogs
 var blogs = new Blogs();
 
+//Declare View in Backbone
 var BlogView = Backbone.View.extend({
-    model: new Blog(),
-    tagName: 'tr',
+    model: new Blog(), //Use Blog Model
+    tagName: 'tr', //This is the html container
+    //Initialize when the view loads
     initialize: function(){
+        //Use template with the class .blogs-list-template
         this.template = _.template($('.blogs-list-template').html());
     },
+    //Register all the events with the classes from the html view
     events: {
         'click .edit-blog': 'edit',
         'click .update-blog': 'update',
         'click .cancel-blog': 'cancel',
         'click .delete-blog': 'delete'
     },
+    //Edit function
     edit: function(){
+        //Hide and show buttons
         $('.edit-blog').hide();
         $('.delete-blog').hide();
         this.$('.update-blog').show();
         this.$('.cancel-blog').show();
 
+        //Store the values from the cells
         var author = this.$('.author').html();
         var title = this.$('.title').html();
         var url = this.$('.url').html();
 
+        //Switch the text for input fields to allow edition
         this.$('.author').html($('<input>', {
             type: 'text',
             class: 'form-control author-update',
@@ -52,11 +65,14 @@ var BlogView = Backbone.View.extend({
             value: url
         }));
     },
+    //Update function
     update: function(){
+        //Set values to the model from the input fields
         this.model.set('author', $('.author-update').val());
         this.model.set('title', $('.title-update').val());
         this.model.set('url', $('.url-update').val());
 
+        //Save to the database
         this.model.save(null, {
             success: function(response){
                 console.log('Successfully updated blog with _id: ' + response.toJSON()._id);
@@ -66,10 +82,14 @@ var BlogView = Backbone.View.extend({
             }
         });
     },
+    //Cancel function
     cancel: function(){
+        //Render the default view when user clicks on cancel edition button
         blogsView.render();
     },
+    //Delete function
     delete: function(){
+        //Destroy model and record from database
         this.model.destroy({
             success: function(response){
                 console.log('Successfully deleted blog with _id: ' + response.toJSON()._id);
@@ -79,26 +99,35 @@ var BlogView = Backbone.View.extend({
             }
         });
     },
+    //Render function
     render: function(){
+        //Get element and put it in the template with the model in JSON format
         this.$el.html(this.template(this.model.toJSON()));
         return this;
     }
 });
 
+//Declare view for all blogs
 var BlogsView = Backbone.View.extend({
-    model: blogs,
-    el: $('.blogs-list'),
+    model: blogs, //Use blogs collection
+    el: $('.blogs-list'), //Get elemento from html with class .blogs-list (tbody)
+    //Initialize
     initialize: function(){
-        var self = this;
+        var self = this; //Store this in self to use in child functions
+        //On add event, render view
         this.model.on('add', this.render, this);
+
+        //On change event, render view but with a 30ms delay to let the application render all the changes
         this.model.on('change', function(){
             setTimeout(function(){
                 self.render();
             }, 30);
         }, this);
 
+        //On remove event, render view
         this.model.on('remove', this.render, this);
 
+        //Fetch all records, all the blogs from database
         this.model.fetch({
             success: function(response){
                 _.each(response.toJSON(), function(item){
@@ -111,29 +140,38 @@ var BlogsView = Backbone.View.extend({
         });
     },
     render: function(){
-        var self = this;
-        this.$el.html('');
+        var self = this; //Store this in self to use in child functions
+        this.$el.html(''); //Empty the element .blogs-list
+        //Append each blog to the element .blogs-list
         _.each(this.model.toArray(), function(blog){
+            //Append each blog as a BlogView using blog model
             self.$el.append((new BlogView({model: blog})).render().$el);
         });
         return this;
     }
 });
 
+//Document ready function
 $(document).ready(function(){
+
+    //On click in .add-blog button
     $('.add-blog').on('click', function(){
+        //Create new Blog model and assign input values
         var blog = new Blog({
             author: $('.author-input').val(),
             title: $('.title-input').val(),
             url: $('.url-input').val()
         });
 
+        //Empty input fields when blog is added
         $('.author-input').val('');
         $('.title-input').val('');
         $('.url-input').val('');
 
+        //Add blog model to blogs collection
         blogs.add(blog);
 
+        //Save to the database
         blog.save(null, {
             success: function(response){
                 console.log('Successfully saved blog with _id: ' + response.toJSON()._id)
@@ -145,4 +183,5 @@ $(document).ready(function(){
     });
 });
 
+//Declare blogsView
 var blogsView = new BlogsView();
--- server.js
+++ server.js
@@ -1,21 +1,28 @@
+//Dependencies
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 
+//Connect MongoDB
 mongoose.connect('mongodb://localhost/blogroll');
 
+//Declare Schema
 var Schema = mongoose.Schema;
 
+//Create new Schema for Blog
 var BlogSchema = new Schema({
     author: String,
     title: String,
     url: String
 });
 
+//Assign Schema to Blog Model on Mongoose
 mongoose.model('Blog', BlogSchema);
 
+//Assign Mongoose Model to Blog
 var Blog = mongoose.model('Blog');
 
+//Create new blog for testing and creation of the database
 // var blog = new Blog({
 //     author: "Michael",
 //     title: "Michael's Blog",
@@ -24,56 +31,75 @@ var Blog = mongoose.model('Blog');
 //
 // blog.save();
 
+//Declare app with express
 var app = express();
 
+//Declare public folder to serve static content
 app.use(express.static(__dirname + '/public'));
 
+//Use bodyparser to handle JSON
 app.use(bodyParser.json());
 
 //Routes
+
+//Home
 app.get('/api/blogs', function(req,res){
+
+    //Get all blogs from database
     Blog.find(function(err, docs){
+        //Console all records
         docs.forEach(function(item){
             console.log('Received a GET request for _id: ' + item._id);
         });
+        //Send records to the view
         res.send(docs);
     });
 });
 
+//Create a new blog
 app.post('/api/blogs', function(req,res){
 
     console.log('Received a POST request');
 
+    //Console log all the request
     for(var key in req.body){
         console.log(key + ': ' + req.body[key]);
     }
 
+    //Create new blog
     var blog = new Blog(req.body);
+
+    //Save blog with Mongoose
     blog.save(function(err, doc){
+        //Send doc to view
         res.send(doc);
     });
 });
 
+//Delete a blog
 app.delete('/api/blogs/:id', function(req, res){
 
     console.log('Received a DELETE request for _id: ' + req.params.id);
 
+    //Blog model remove item
     Blog.remove({_id: req.params.id}, function(err){
         res.send({_id: req.params.id});
     });
 });
 
+//Update a blog
 app.put('/api/blogs/:id', function(req,res){
 
     console.log('Received an UPDATE request for _id: ' + req.params.id);
 
+    //Blog model update item
     Blog.update({_id: req.params.id}, req.body, function(err){
         res.send({_id: req.params.id});
     });
 });
 
+//Listen on port 3000
 var port = 3000;
-
 app.listen(port);
 
-console.log('Server on ' + port);
+console.log('Server on http://localhost:' + port);
